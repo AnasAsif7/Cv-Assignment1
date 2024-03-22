9b54184e166bb7a58b878b57e2fce2e72ed592cc
AnasAsif7 <161208637+AnasAsif7@users.noreply.github.com>
2024-03-23 03:23:22 +0400

    signupregexfix

--- gamingPage/signup.js
+++ gamingPage/signup.js
@@ -1,49 +1,52 @@
 $(document).ready(function() {
     $('#signupForm').submit(function(e) {
-        e.preventDefault(); // Always prevent the default first
+        e.preventDefault(); 
 
-        var isValid = true; // Variable to keep track of overall form validity
+        var isValid = true; 
 
-        // Validate Username
+        
         var username = $('#username').val();
         if(username.length < 3) {
             alert('Username must be at least 3 characters long');
-            isValid = false; // Update the form validity
+            isValid = false; 
         }
 
-        // Validate Email
+        
         var email = $('#email').val();
         var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!emailRegex.test(email)) {
             alert('Please enter a valid email address');
-            isValid = false; // Update the form validity
+            isValid = false; 
         }
 
-        // Validate Password
+        
         var password = $('#password').val();
         if(password.length < 6) {
             alert('Password must be at least 6 characters long');
-            isValid = false; // Update the form validity
+            isValid = false; 
         }
 
-        // Validate Confirm Password
+        
         var confirmPassword = $('#confirmPassword').val();
         if(password !== confirmPassword) {
             alert('Passwords do not match');
-            isValid = false; // Update the form validity
+            isValid = false; 
         }
 
-        // Validate Phone Number
+        
         var phone = $('#phone').val();
-        var phoneRegex = /^[0-9]{10}$/;
-        if(!phoneRegex.test(phone)) {
-            alert('Please enter a valid 10-digit phone number');
-            isValid = false; // Update the form validity
+        var phoneRegex = /^(03)[0-9]{9}$/;
+        
+        if (!phoneRegex.test(phone)) {
+            alert('Please enter a valid phone number');
+            isValid = false;
         }
+        
+        
 
-        // If all validations pass, submit the form
+        
         if(isValid) {
-            this.submit(); // Use the native submit function of the form
+            this.submit(); 
         }
     });
 });
