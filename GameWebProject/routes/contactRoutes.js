d03a1330eee9fed39143d0bc7b5186aa13cb9fae
AnasAsif7 <161208637+AnasAsif7@users.noreply.github.com>
2024-05-26 21:55:32 +0500

    chng

--- GameWebProject/routes/contactRoutes.js
+++ GameWebProject/routes/contactRoutes.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Contact = require('../models/Contact');
+const Contact = require("../models/Contact");
 const sessionAuth = require("../middleware/sessionAuth");
 
 // Middleware to check if user is admin
@@ -13,26 +13,32 @@ function isAdmin(req, res, next) {
 }
 
 // Handle contact form submission
-router.post('/contact', async (req, res) => {
+router.post("/contact", async (req, res) => {
   const { firstName, lastName, email, subject, message } = req.body;
-  
+
   try {
-    const newContact = new Contact({ firstName, lastName, email, subject, message });
+    const newContact = new Contact({
+      firstName,
+      lastName,
+      email,
+      subject,
+      message,
+    });
     await newContact.save();
-    res.status(201).send('Message sent successfully');
+    res.status(201).send("Message sent successfully");
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
-router.get('/customer-requests',isAdmin, async (req, res) => {
-    try {
-        const contacts = await Contact.find().sort({ createdAt: -1 }); // Sorting by newest first
-        res.render('customerRequests', { contacts });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
+router.get("/customer-requests", isAdmin, async (req, res) => {
+  try {
+    const contacts = await Contact.find().sort({ createdAt: -1 }); // Sorting by newest first
+    res.render("customerRequests", { contacts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
